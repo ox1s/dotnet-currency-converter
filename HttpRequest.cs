7c96ce1d0c062b58925527bf62305dfb063b679e
ox1s <linamel08v@gmail.com>
2025-10-22 11:47:50 +0300

    refactor: HttpRequest

--- HttpRequest.cs
+++ HttpRequest.cs
@@ -3,15 +3,13 @@ using System.Net.Http.Json;
 public class HttpRequest
 {
     private readonly HttpClient _client;
-    private readonly string _url;
-    public HttpRequest(HttpClient client, string url)
+    public HttpRequest(HttpClient client)
     {
         _client = client;
-        _url = url;
     }
-    public async Task<List<Currency>> SendRequest()
+    public async Task<List<Currency>> SendRequest(string _url)
     {
-        var currencies = await _client.GetFromJsonAsync<List<Currency>>(_url);
+        var repositories = await _client.GetFromJsonAsync<List<Currency>>(_url);
 
         if (repositories is null || repositories.Count == 0)
             throw new InvalidOperationException("Невозможно получить данные о валютах");
--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ client.DefaultRequestHeaders.Accept.Add(
 client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
 
-var request = new HttpRequest(client, "https://api.nbrb.by/exrates/rates?periodicity=0");
-var response = await request.SendRequest();
+var request = new HttpRequest(client);
+var response = await request.SendRequest("https://api.nbrb.by/exrates/rates?periodicity=0");
 // string[] currencies = { "USD", "EUR", "RUB" };
 string[] currencies = response.Select(x => x.Cur_Abbreviation).ToArray();
 // Выбор стоимости
